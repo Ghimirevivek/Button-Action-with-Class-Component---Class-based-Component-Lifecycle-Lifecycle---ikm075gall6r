2c689185b4480bd7daf56e97231f5c0c65bdf89a
Ghimirevivek <stephengmre@gmail.com>
2023-03-18 20:22:06 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from 'react'
+import '../styles/App.css'
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
+  constructor(props) {
+    super(props)
+    this.state = {
+      isClicked: false,
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true })
+  }
+
+  render() {
+    return (
+      <div id="main">
+        {/* Add button with onClick event to update state */}
+        <button id="click" onClick={this.handleClick}>
+          Click me
+        </button>
+
+        {/* Add conditional rendering based on state */}
+        {this.state.isClicked && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
+      </div>
+    )
+  }
 }
 
-
-export default App;
-
+export default App
